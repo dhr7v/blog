ca64f75ca24692055a4181398b7efca26dbe2abc
burman <me@dhruvburman.com>
2024-02-10 18:01:29 +0530

    idevelopstuff

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "dhruv",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
-    bio: "I develop.",
+    bio: "I develop stuff.",
     email: "me@dhruvburman.com",
     linkedin: "dhruvburman",
     github: "dhruvburman97",
